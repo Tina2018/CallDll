6491175531e669fdb25e0b3900667a306da9d22d
Tina <919457987@qq.com>
2020-06-22 03:38:07 +0000

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cla = new Class1();
+            cla.Test(); //set bp here
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
